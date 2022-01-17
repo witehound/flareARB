83d668740bb40dd1590439a32376608ec6c399cd
InventedMost <netvid00@gmail.com>
2022-01-17 14:59:16 +0100

    v2.0
    user can run 2 different arbitrage

--- arb.js
+++ arb.js
@@ -6,11 +6,17 @@ let sgbinit = document.querySelector(".sgbstart")
 const sgbFinale = document.querySelector(".sgbinitp")
 const sgbFinalep = document.querySelector(".sgbfinalep")
 const displayStatus = document.querySelector(".sgbstatus")
-let initexfi
-let initusdt
-let sgbfinal
-let endresult
-let sgbinitt
+let initexfi, initusdt, sgbfinal, endresult, sgbinitt
+
+const valueoneback = document.querySelector(".bitrueusdtback")
+const valuetwoback = document.querySelector(".bitrueexfiback")
+const valuethreeback = document.querySelector(".flaresgbback")
+const button2 = document.querySelector(".btncrunch2")
+let sgbinitback = document.querySelector(".sgbstartback")
+const sgbFinaleback = document.querySelector(".sgbinitpback")
+const sgbFinalepback = document.querySelector(".sgbfinalepback")
+const displayStatusback = document.querySelector(".sgbstatusback")
+let initexfiback, initusdtback, sgbfinalback, endresultback, sgbinittback
 
 
 button.addEventListener("click", function(){
@@ -22,5 +28,17 @@ button.addEventListener("click", function(){
     sgbFinale.innerHTML = sgbinitt
     sgbFinalep.innerHTML = sgbfinal
     displayStatus.innerHTML = endresult
+})
+
+button2.addEventListener("click", function(){
+    initexfiback = sgbinitback.value * valueoneback.value
+    initusdtback = initexfiback / valuetwoback.value
+    let exfiDeduct = initusdtback - 0.1
+    sgbfinalback = exfiDeduct * valuethreeback.value
+    sgbinittback =  sgbinitback.value
+    endresultback = sgbfinalback - sgbinittback
+    sgbFinaleback.innerHTML = sgbinittback
+    sgbFinalepback.innerHTML = sgbfinalback
+    displayStatusback.innerHTML = endresultback
 })
 
